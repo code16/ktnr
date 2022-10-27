b093f9b6899caeb759a119594a1eab78d405f9dd
antoine <antoine.guingand@wanadoo.fr>
2022-10-27 15:50:16 +0200

    Remove animated-icon-menu.blade.php

--- source/_components/ui/animated-icon-menu.blade.php
+++
@@ -1,7 +0,0 @@
-<div class="relative w-6 h-6">
-    <div class="block w-full absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2">
-        <span aria-hidden="true" class="block absolute h-0.5 w-full bg-current transform transition duration-500 ease-in-out" :class="{'rotate-45': open,' -translate-y-2': !open }"></span>
-        <span aria-hidden="true" class="block absolute h-0.5 w-full bg-current transform transition duration-500 ease-in-out" :class="{'opacity-0': open } "></span>
-        <span aria-hidden="true" class="block absolute h-0.5 w-full bg-current transform transition duration-500 ease-in-out" :class="{'-rotate-45': open, 'translate-y-2': !open}"></span>
-    </div>
-</div>
