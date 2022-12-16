6e5023917c29513bbb14af3188c77b75f294079c
antoine <antoine.guingand@wanadoo.fr>
2022-12-16 09:06:37 +0100

    Remove label for now

--- source/_components/layout/events.blade.php
+++ source/_components/layout/events.blade.php
@@ -21,7 +21,7 @@
             <x-event-item :event="$event" :light="$loop->even" />
         @endforeach
     @else
-        Aucun événement pour le moment !
+{{--        Aucun événement pour le moment !--}}
     @endif
 </div>
 
